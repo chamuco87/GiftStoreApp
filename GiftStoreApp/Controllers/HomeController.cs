58dc68aaa0ec9b334fbb68fff8485f51be71aa91
Jose Carlos Carbajal Salinas <jose.carbajal.salinas@gmail.com>
2018-03-18 16:03:29 -0400

    Registering data

--- GiftStoreApp/Controllers/HomeController.cs
+++ GiftStoreApp/Controllers/HomeController.cs
@@ -5,6 +5,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using GiftStoreApp.Models;
+using GiftStoreApp.ViewModels;
 
 namespace GiftStoreApp.Controllers
 {
@@ -14,8 +15,32 @@ namespace GiftStoreApp.Controllers
         {
             GiftStoreAppContext dbContext = new GiftStoreAppContext();
             var setting = dbContext.GlobalSettings.FirstOrDefault();
+            return View("Login");
+        }
+
+        public IActionResult Register()
+        {
+            GiftStoreAppContext dbContext = new GiftStoreAppContext();
+            var setting = dbContext.GlobalSettings.FirstOrDefault();
             return View("Register");
         }
+
+        [HttpPost]
+        public IActionResult Register(SignUpViewModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                GiftStoreAppContext dbContext = new GiftStoreAppContext();
+                var setting = dbContext.GlobalSettings.FirstOrDefault();
+                return Ok(model);
+            }
+            else{
+
+                return BadRequest(ModelState);
+            }
+
+        }
+
 
         public IActionResult Error()
         {
